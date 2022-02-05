21766c5398d53df1c51ce734ed98c8ef03404687
Sue Yang <sueyang112@hotmail.com>
2022-02-05 12:50:45 -0500

    Added Session portion.)

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect  # added request
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-# our index route will handle rendering our form
+app.secret_key = 'keep it secret, keep it safe'
 
 
 @app.route('/')
@@ -11,10 +11,15 @@ def index():
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
-    return redirect('/')
+    # Here we add two properties to session to store the name and email
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html')
 
 
 if __name__ == "__main__":
